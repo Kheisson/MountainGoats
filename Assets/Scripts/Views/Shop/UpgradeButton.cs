8d5c5cf05e0cee1093f7ecb9f45f720a303196f2
Kheisson <igor.kheison@gmail.com>
2025-05-09 21:56:14 +0300

    remove weird comma

--- Assets/Scripts/Views/Shop/UpgradeButton.cs
+++ Assets/Scripts/Views/Shop/UpgradeButton.cs
@@ -59,7 +59,7 @@ namespace Views.Shop
             _upgradePath = upgradePath;
             _upgradeType = upgradeType;
 
-            costText.text = $"UPGRADE'\n'{upgradePath.AvailableUpgrades[_buttonIndex].Cost}$";
+            costText.text = $"UPGRADE\n{upgradePath.AvailableUpgrades[_buttonIndex].Cost}$";
         }
 
         public void OnPointerEnter(PointerEventData eventData)
